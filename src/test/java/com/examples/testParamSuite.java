3559e1b510b7cc7eec3f658e29a754cc2a330b32
Abdul Kadir Moğul <158027198+kadirmogul@users.noreply.github.com>
2024-12-09 17:43:58 +0300

    Update testParamSuite.java

--- src/test/java/com/examples/testParamSuite.java
+++ src/test/java/com/examples/testParamSuite.java
@@ -1,8 +1,15 @@
+package com.examples;
+
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class testParamSuite {
-    @Test
-    public void test(){
 
+    @Parameters({"browser","baseURL"})
+    @Test
+    public void test01(String browser,String baseURL){
+        System.out.println(browser);
+        System.out.println(baseURL);
     }
 }
+
