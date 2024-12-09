9c7abd96a18d04968e059fd2f5690b211c01ab1d
Abdul Kadir Moğul <158027198+kadirmogul@users.noreply.github.com>
2024-12-09 17:43:49 +0300

    Update groupExample.java

--- src/test/java/com/examples/groupExample.java
+++ src/test/java/com/examples/groupExample.java
@@ -1,4 +1,15 @@
 package com.examples;
 
+import org.testng.annotations.Test;
+
 public class groupExample {
+   @Test(groups = {"Duman","Regresyon"})
+   public void test01(){
+       System.out.println("Smoke test");
+   }
+   @Test(groups = "Duman")
+    public void test02(){
+       System.out.println("Regresyon test");
+   }
+
 }
