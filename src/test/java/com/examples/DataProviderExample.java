9d0627961f8deeca568d217cd7ccbb9cb8db8bf3
Abdul Kadir Moğul <158027198+kadirmogul@users.noreply.github.com>
2024-12-09 17:43:23 +0300

    Update DataProviderExample.java

--- src/test/java/com/examples/DataProviderExample.java
+++ src/test/java/com/examples/DataProviderExample.java
@@ -1,4 +1,22 @@
 package com.examples;
 
-public class DataProvider {
-}
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DataProviderExample {
+   @DataProvider(name = "testData")
+   public Object[][] list(){
+       return new Object[][]{
+               {23,"Ahmet"},
+               {15,"Ali"},
+               {18,"Aliye"}
+       };
+   }
+    @Test(dataProvider = "testData")
+    public void testMethod(int age,String name){
+        System.out.println("Name: "+name+" Age: "+age);
+
+    }
+
+    }
+
