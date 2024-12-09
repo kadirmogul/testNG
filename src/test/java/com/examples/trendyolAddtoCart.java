8b10d8a23eefabea76ba692e753f2c4659afc316
Abdul Kadir Moğul <158027198+kadirmogul@users.noreply.github.com>
2024-12-09 17:44:34 +0300

    Update trendyolAddtoCart.java

--- src/test/java/com/examples/trendyolAddtoCart.java
+++ src/test/java/com/examples/trendyolAddtoCart.java
@@ -1,31 +1,53 @@
+package com.examples;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import java.security.Key;
 import java.time.Duration;
 import java.util.List;
 
-public class trendyolAddtoCart {
+public class trendyolAddtoCart extends BaseTest{
 
-    WebDriver driver;
-    @BeforeClass
-    public void setUp(){
-        WebDriverManager.chromedriver().setup();
-        driver=new ChromeDriver();
+    @Parameters ({"browser"})
+     public void setUp(String browser){
+
+        switch (browser){
+        case "chrome":
+            WebDriverManager.chromedriver().setup();
+            driver=new ChromeDriver();
+            break;
+        case "firefox":
+            WebDriverManager.firefoxdriver().setup();
+            driver=new FirefoxDriver();
+            break;
+        case "edge":
+            WebDriverManager.edgedriver().setup();
+            driver=new EdgeDriver();
+            break;
+            default:
+                try {
+                    throw new IllegalAccessException("hatalı browser verisi");
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+        }
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
         driver.manage().window().maximize();
 
     }
-    @Test(priority = 1, description = "Arama sonucu",timeOut = 5000, invocationCount = 2)
+    @Test(priority = 1, description = "Arama sonucu",timeOut = 10000, invocationCount = 1)
     public void test01(){
         driver.get("https://www.trendyol.com/");
        WebElement searchButton= driver.findElement(By.className("V8wbcUhU"));
@@ -33,7 +55,7 @@ public class trendyolAddtoCart {
         searchButton.sendKeys(Keys.ENTER);
         String actualDate="sonuç listeleniyor";
         String expectedData=driver.findElement(By.className("dscrptn-V2")).getText();
-        Assert.assertTrue(expectedData.toLowerCase().contains((actualDate).toLowerCase()));
+        softAssert.assertTrue(expectedData.toLowerCase().contains((actualDate).toLowerCase()));
     }
     @Test(priority = 2, description = "Sepete ekleme")
     public void test02() throws InterruptedException {
@@ -53,11 +75,8 @@ public class trendyolAddtoCart {
        actions.perform();
 
       String totalProduct= driver.findElement(By.className("basket-item-count-container")).getText();
-      Assert.assertFalse(totalProduct.equals("0"));
+        softAssert.assertFalse(totalProduct.equals("0"));
 
     }
-    @AfterClass
-    public void tearDown(){
-        driver.quit();
-    }
+
 }
