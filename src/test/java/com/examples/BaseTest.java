4f079f42f23e98844bcdc1d271550b7fce8e5344
Abdul Kadir Moğul <158027198+kadirmogul@users.noreply.github.com>
2024-12-09 17:43:13 +0300

    Update BaseTest.java

--- src/test/java/com/examples/BaseTest.java
+++ src/test/java/com/examples/BaseTest.java
@@ -1,4 +1,30 @@
 package com.examples;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
+
 public class BaseTest {
+    WebDriver driver;
+    SoftAssert softAssert;
+
+    @BeforeClass
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        softAssert=new SoftAssert();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+        softAssert.assertAll();
+    }
 }
